40f78f2bc491dc92339477c503634a251c674621
De Villiers <ddv@sun.ac.za>
2023-06-26 20:15:33 +0200

    com: fixed confusing comment

--- utils/Horiz2PhTh.m
+++ utils/Horiz2PhTh.m
@@ -1,6 +1,6 @@
 function [ph,th] = Horiz2PhTh(az,alt)
 % function [ph,th] = Horiz2PhTh(az,alt)
-% Converts horizontal coordinates (az from north and alt from zenith) to 
+% Converts horizontal coordinates (az from north and alt from horison) to 
 % local [ph,th] coordinates.
 ph = wrap2pi(-az);
 th = wrap2pi(pi/2 - alt);
